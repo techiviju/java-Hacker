b96a565bc5782ef9901da1effd74875e3088ea49
vijay chaudhari <113278961+techiviju@users.noreply.github.com>
2023-12-23 11:36:14 +0530

    Initialization.java

--- Initialization.java
+++ Initialization.java
@@ -49,5 +49,14 @@ static {
   System.exit(0);
  }
 }
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
 
 	
